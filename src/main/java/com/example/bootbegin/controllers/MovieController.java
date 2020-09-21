a95724278fa250de07ce5bd9152a839f0eaf7039
TarikoDan <62624974+TarikoDan@users.noreply.github.com>
2020-09-21 18:44:25 +0300

    AutoWired

--- src/main/java/com/example/bootbegin/controllers/MovieController.java
+++ src/main/java/com/example/bootbegin/controllers/MovieController.java
@@ -3,6 +3,7 @@ package com.example.bootbegin.controllers;
 import com.example.bootbegin.dao.MovieDao;
 import com.example.bootbegin.entiti.Movie;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.List;
@@ -24,17 +25,45 @@ public class MovieController {
         return movieDao.findById(id).orElseThrow(() -> new NullPointerException("there is no Movie with Id " + id));
     }
     @PostMapping
+    @ResponseStatus(HttpStatus.CREATED)
     public Movie insertMovie(@RequestBody Movie movie) {
         return movieDao.save(movie);
     }
     @PutMapping("/{id}")
     public Movie editMovie (@PathVariable int id, @RequestBody Movie movie) {
-        Movie one = movieDao.getOne(id);
-        one.setTitle(movie.getTitle());
-        one.setDuration(movie.getDuration());
-        movieDao.flush();
-        movie.setId(id);
-        return movie;
+//        Movie one = movieDao.getOne(id);
+//        one.setTitle(movie.getTitle());
+//        one.setDuration(movie.getDuration());
+//        movieDao.flush();
+        if (movieDao.existsById(id)) {
+            movie.setId(id);
+            movieDao.saveAndFlush(movie);
+            return movie;
+        }
+        else {
+            return null;
+        }
+
+    }
+    @DeleteMapping
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteMovie (@RequestBody Movie movie) {
+        List<Movie> movies = movieDao.findAll();
+        for (Movie m : movies) {
+            if ((movie.getTitle().equals(m.getTitle()) && (movie.getDuration() == m.getDuration()))) {
+                movieDao.delete(m);
+            }
+        }
+    }
+    @DeleteMapping("/{id}")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void deleteByID (@PathVariable int id) {
+        movieDao.deleteById(id);
+    }
+    @DeleteMapping("/clea")
+    @ResponseStatus(HttpStatus.NO_CONTENT)
+    public void clearAll() {
+        movieDao.deleteAllInBatch();
     }
 
 }
