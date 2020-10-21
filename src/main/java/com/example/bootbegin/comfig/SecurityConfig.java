09bbb074aa47604b531bb3574a58f75c651ace8a
TarikoDan <62624974+TarikoDan@users.noreply.github.com>
2020-10-21 17:26:50 +0300

    SecurityConfig with BasicAuthorization

---
+++ src/main/java/com/example/bootbegin/comfig/SecurityConfig.java
@@ -0,0 +1,34 @@
+package com.example.bootbegin.comfig;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.http.HttpMethod;
+import org.springframework.security.config.annotation.authentication.builders.AuthenticationManagerBuilder;
+import org.springframework.security.config.annotation.web.builders.HttpSecurity;
+import org.springframework.security.config.annotation.web.configuration.EnableWebSecurity;
+import org.springframework.security.config.annotation.web.configuration.WebSecurityConfigurerAdapter;
+import org.springframework.security.crypto.password.NoOpPasswordEncoder;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+@EnableWebSecurity
+public class SecurityConfig extends WebSecurityConfigurerAdapter {
+
+    @Bean
+    public PasswordEncoder passwordEncoder() {
+        return NoOpPasswordEncoder.getInstance();
+    }
+
+    @Override
+    protected void configure(AuthenticationManagerBuilder auth) throws Exception {
+        auth.inMemoryAuthentication().withUser("user").password("user").roles("USER")
+        .and().withUser("admin").password("admin").roles("ADMIN");
+    }
+
+    @Override
+    protected void configure(HttpSecurity http) throws Exception {
+        http.csrf().disable().cors().disable().authorizeRequests()
+                .antMatchers(HttpMethod.GET).authenticated()
+                .antMatchers(HttpMethod.POST).hasRole("ADMIN")
+                .antMatchers(HttpMethod.POST, "/users").anonymous()
+                .and().httpBasic();
+    }
+}
